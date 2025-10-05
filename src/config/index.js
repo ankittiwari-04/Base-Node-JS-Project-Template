8b073380d4943e7ff7c2f08f631e19aa811b6e81
ankittiwari-04 <ankit8127171653@gmail.com>
2025-10-06 01:23:23 +0530

    Added basic node express functionalities

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+logger = require('./logger-config');
+
+module.exports={
+  serverConfig : require('./server-config'),
+  logger: require('./logger-config')
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { serverConfig,logger } = require('./config');
+const apiRoutes =require('./routes');
+
+
+const app = express();
+
+app.use('/api',apiRoutes);
+
+app.listen(serverConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT : ${serverConfig.PORT}`);
+    logger.info("Succesfully started the server ",{});
+});
