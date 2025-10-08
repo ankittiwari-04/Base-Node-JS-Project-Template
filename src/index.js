baa9734f75ba76d448dd8bcf16510df9c9ebb248
ankittiwari-04 <ankit8127171653@gmail.com>
2025-10-09 00:56:44 +0530

    Added new migration to add foreign key constraints in one to many relation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 const express = require('express');
-
-const { serverConfig,logger } = require('./config');
-const apiRoutes =require('./routes');
-
+const { serverConfig } = require('./config'); // fixed capitalization
+const apiRoutes = require('./routes');
+const { City, Airport } = require('./models'); // use ./ instead of ../
+ // require models at the top
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api',apiRoutes);
+// index.js > app.listen callback
+app.use('/api', apiRoutes);
 
-app.listen(serverConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${serverConfig.PORT}`);
-    logger.info("Succesfully started the server ",{});
+app.listen(serverConfig.PORT, async () => {
+    console.log(`Successfully started the server on PORT: ${serverConfig.PORT}`);
+    
+    // bad code alert
+    // const { City, Airport } = require('../models');
+    // const bengaluru = await City.findByPk(1, {include: {model: Airport}})
+    // console.log(bengaluru);
+    // const airport = await Airport.create({ name: 'Kempegowda Airport', code: 'BLR'})
+    // const dbairport = await bengaluru.createAirport({name: 'Huballi Airport'})
+    // console.log(dbairport);
+    // const airportsInBlr = await bengaluru.getAirports();
+    // console.log(airportsInBlr);
+    // const hbairport = await Airport.findByPk(3);
+    // console.log(hbairport);
+    // await bengaluru.removeAirports(hbairport);
+    // const mumbai = await City.findByPk(2);
+    // const sh = await mumbai.createAirport({name: 'CSI airport', code: 'MUM'});
+   // await City.destroy({
+     //   where: {
+       //     id: 2
+        //}
+ // // });
+// // const sh = await Airport.findByPk(2);
+// // mumbai.removeAirport(sh)
+// // const city = await City.findByPk(4);
+// // await city.createAirport({name: 'Indore airport', code: 'IND'});
+await City.destroy({
+    where: {
+        id: 4
+    }
+});
 });
